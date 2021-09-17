82a282811011c44df6b5ef186c37f8934bc07047
Benjamin Wertenberger <bwertenberger52@gmail.com>
2021-09-17 00:12:43 -0500

    Bug Fixes and bouncing knife
    Fixed bug pertaining to knife duping. Also added bouncing feature. Hit-boxes updated to encourage this feature.

---
+++ Rogue Like Bullet Hell/Assets/Scripts/bullet.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    public Rigidbody2D knife;
+    public PhysicsMaterial2D bounce;
+    public GameObject player;
+    bool hasBroke = false;
+    public GameObject hitEffect;
+    public float change;
+    bool hasBounce = false;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(!hasBounce)
+        {
+            bounce.bounciness = change;
+            hasBounce = true;
+        }
+        if (collision.collider.name.Contains("Player"))
+        {
+            Destroy(gameObject);
+            yeeting.hasYoted = false;
+        }
+        else if (collision.collider.name.Contains("slime"))
+        {
+            Debug.Log(bounce.bounciness);
+            bounce.bounciness = bounce.bounciness + change;
+        }
+        else if (collision.collider.name.Contains("Wall"))
+        {
+            bounce.bounciness = 0;
+            knife.bodyType = RigidbodyType2D.Static;
+        }
+
+
+        
+      
+    }
+}
